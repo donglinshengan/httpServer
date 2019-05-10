cbcde233ab0dd3344851dd733b364b005a1ddecf
donglinshengan <donglinshengan@163.com>
2019-05-09 19:05:25 -0700

    modify sh

--- startServer.sh
+++ startServer.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+let n=$(service iptables status | grep -ce "Stopping .*")
+let z=0
+if [ "${n}" -eq "${z}" ]
+then
+echo "the firewall is working"
 service iptables start
 service iptables stop
+else 
+echo "the firewall is stopped"
+fi
 
 make
 
